a91fffc9c296be83f8047b7976ec9d74d7f4bb74
E. Misael Ortiz <E. Misael Ortiz@VAIO>
2018-04-15 17:48:52 -0300

    Punto 2

---
+++ src/tp3/punto2/TestRectagulo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp3.punto2;
+
+/**
+ *
+ * @author E. Misael Ortiz
+ */
+public class TestRectagulo {
+    public static void main(String[] args) {
+        
+    }
+}
