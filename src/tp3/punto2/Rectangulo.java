71590ae58bc78316ad0ee1aece14b403b544f5b2
E. Misael Ortiz <E. Misael Ortiz@VAIO>
2018-04-15 17:47:10 -0300

    Creando Clase Rectangulo, Punto 2

---
+++ src/tp3/punto2/Rectangulo.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp3.punto2;
+
+/**
+ *
+ * @author E. Misael Ortiz
+ */
+public class Rectangulo {
+    private double puntox;
+    private double puntoy;
+    private double base;
+    private double altura;
+
+    /**
+     * @return the puntox
+     */
+    public double getPuntox() {
+        return puntox;
+    }
+
+    /**
+     * @param puntox the puntox to set
+     */
+    public void setPuntox(double puntox) {
+        this.puntox = puntox;
+    }
+
+    /**
+     * @return the puntoy
+     */
+    public double getPuntoy() {
+        return puntoy;
+    }
+
+    /**
+     * @param puntoy the puntoy to set
+     */
+    public void setPuntoy(double puntoy) {
+        this.puntoy = puntoy;
+    }
+
+    /**
+     * @return the base
+     */
+    public double getBase() {
+        return base;
+    }
+
+    /**
+     * @param base the base to set
+     */
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    /**
+     * @return the altura
+     */
+    public double getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
