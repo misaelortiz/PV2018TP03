6eef68317d3bb75871927f2a62fef3719d21ee94
Alumno <Alumno@2CIDC85DE6359BC>
2018-04-15 22:28:25 -0300

    punto7

---
+++ src/tp3/punto7/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp3.punto7;
+
+import java.util.Scanner;
+import tp3.punto7.Coche;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Principal {
+    
+    public Coche[] coches = new Coche[4];
+    public static void main(String[] args) {
+      
+    Coche unCoche;
+        unCoche = new Coche();
+    
+    }
+    
+}
