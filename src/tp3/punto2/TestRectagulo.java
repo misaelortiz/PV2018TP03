57327ee87d1f4d9dda1714d0dc1efadca9fde6b8
E. Misael Ortiz <E. Misael Ortiz@VAIO>
2018-04-16 12:19:28 -0300

    Llamado de cada metodo.

--- src/tp3/punto2/TestRectagulo.java
+++ src/tp3/punto2/TestRectagulo.java
@@ -14,18 +14,26 @@ import java.util.Scanner;
 public class TestRectagulo {
     public static void main(String[] args) {
         Rectangulo puntox1 = new Rectangulo();
-        Rectangulo puntox2 = new Rectangulo();
+        Rectangulo puntoy1 = new Rectangulo();
         Rectangulo base= new Rectangulo();
         Rectangulo altura=new Rectangulo();
+        Rectangulo diagonal=new Rectangulo();
+        Rectangulo v2 = new Rectangulo();
+        Rectangulo v3 = new Rectangulo();
+        Rectangulo v4 = new Rectangulo();
+        Rectangulo s = new Rectangulo();
+        Rectangulo p = new Rectangulo();
+        
         Scanner sc = new Scanner (System.in);
         double x1;
-        System.out.println("Ingrese x del punto del vertice: ");
+        System.out.println("Ingrese X del punto del vertice 1: ");
         x1=sc.nextDouble();
         puntox1.setPuntox1(x1);
         double y1;
-        System.out.println("Ingrese y del punto del vertice: ");
+        System.out.println("Ingrese Y del punto del vertice 1: ");
         y1=sc.nextDouble();
-        puntox2.setPuntoy1(y1);
+        puntoy1.setPuntoy1(y1);
+        
         double b;
         System.out.println("Ingrese base del rectangulo: ");
         b=sc.nextDouble();
@@ -34,7 +42,17 @@ public class TestRectagulo {
         System.out.println("Ingrese altura del rectangulo: ");
         h=sc.nextDouble();
         altura.setAltura(h);
-        
+        double d;
+        d=diagonal.ObtenerDiagonal(b, h);
+        System.out.println("La diagonal del rectangulo es:  " + d );
+        System.out.println("Vertice1= x: " + x1 + " y: " + y1 );
+        v2.ObtenerVertice2(puntox1,puntoy1, altura);
+        v3.ObtenerVertice3(puntox1, puntoy1, base, altura);
+        v4.ObtenerVertice4(puntoy1,puntox1, base);
+        //s.ObtenerSuperficie(base, altura);
+        System.out.println("La superficie del rectangulo es: " +s.ObtenerSuperficie(base, altura) );
+        //p.ObtenerPerimetro(base, altura);
+        System.out.println("El perimetro del rectangulo es: " + p.ObtenerPerimetro(base, altura));
         
     }
 }
