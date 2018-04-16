ca14ad14eedf8a0de822aeaf72ff5447f68898ea
E. Misael Ortiz <E. Misael Ortiz@VAIO>
2018-04-16 12:49:48 -0300

    Punto 3: Creacion del metodo para saber si se puede o no formar un triangulo con 3 lados ingresados por el usuario.

--- src/tp3/punto3/Triangulo.java
+++ src/tp3/punto3/Triangulo.java
@@ -10,5 +10,68 @@ package tp3.punto3;
  * @author E. Misael Ortiz
  */
 public class Triangulo {
+    private double lado1;
+
+    public Triangulo() {
+    }
+    private double lado2;
+    private double lado3;
+
+    /**
+     * @return the lado1
+     */
+    public double getLado1() {
+        return lado1;
+    }
+
+    /**
+     * @param lado1 the lado1 to set
+     */
+    public void setLado1(double lado1) {
+        this.lado1 = lado1;
+    }
+
+    /**
+     * @return the lado2
+     */
+    public double getLado2() {
+        return lado2;
+    }
+
+    /**
+     * @param lado2 the lado2 to set
+     */
+    public void setLado2(double lado2) {
+        this.lado2 = lado2;
+    }
+
+    /**
+     * @return the lado3
+     */
+    public double getLado3() {
+        return lado3;
+    }
+
+    /**
+     * @param lado3 the lado3 to set
+     */
+    public void setLado3(double lado3) {
+        this.lado3 = lado3;
+    }
     
+    public void ResolverTriangulo(Triangulo a,Triangulo b, Triangulo c){
+        double p;
+        if (a.getLado1() + b.getLado2() > c.getLado3() )
+            if (a.getLado1() + c.getLado3() > b.getLado2())
+                if(b.getLado2()+c.getLado3() > a.getLado1())
+                {   p=a.getLado1()+b.getLado2()+c.getLado3();
+                    System.out.println("Si se forma un triangulo y su perimetro es: " + p);
+                }
+            else
+                 System.out.println("No es posible formar un triangulo");
+        else
+                System.out.println("No es posible formar un triangulo");
+        
+        
+    }
 }
