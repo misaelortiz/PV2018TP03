400b23ce429aacb01a856ad053e4f1a2d73ba644
E. Misael Ortiz <E. Misael Ortiz@VAIO>
2018-04-15 23:22:01 -0300

    Punto 5 , Clase TestPunto5

---
+++ src/tp3/punto5/test/TestPunto5.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp3.punto5.test;
+
+import tp3.punto5.modelo.Circulo;
+import tp3.punto5.utils.GestorCirculos;
+
+/**
+ *
+ * @author candeluchi
+ */
+public class TestPunto5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Circulo c1 = new Circulo(3.4, "Rojo");
+        Circulo c2 = new Circulo(5.4, "Verde");
+        
+        GestorCirculos gc = new GestorCirculos();
+        gc.agregarCirculo(c1);
+        gc.agregarCirculo(c2);
+        
+        for(Circulo circulo: gc.getCirculos()){
+            System.out.println("Radio:" + circulo.getRadio());
+            System.out.println("Color:" + circulo.getColor());
+        }
+        
+    }
+    
+}
