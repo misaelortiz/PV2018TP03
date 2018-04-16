f5b2d7da250c31b0f1ba894ae5f348f6efb216dd
E. Misael Ortiz <E. Misael Ortiz@VAIO>
2018-04-16 13:09:19 -0300

    Punto 8. Creacion de la clase Libro.

---
+++ src/tp3/punto8/modelo/Libro.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp3.punto8.modelo;
+
+/**
+ *
+ * @author E. Misael Ortiz
+ */
+public class Libro {
+    private int isbn;
+
+    public Libro() {
+    }
+    private String titulo;
+    private String autor;
+    private double precio;
+
+    /**
+     * @return the isbn
+     */
+    public int getIsbn() {
+        return isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the autor
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     * @param autor the autor to set
+     */
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+}
