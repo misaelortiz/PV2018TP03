374fa49cd251f83398b660c602a3902a008822e2
Alumno <Alumno@2CIDC85DE6359BC>
2018-04-15 21:53:31 -0300

    punto4

--- src/tp3/punto4/Principal.java
+++ src/tp3/punto4/Principal.java
@@ -5,10 +5,28 @@
  */
 package tp3.punto4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alumno
  */
 public class Principal {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArregloNumero arreglo = new ArregloNumero();
+        int contador =0;
+        while (contador<10){
+            System.out.println("Ingrese un numero: ");
+            arreglo.getNumeros()[contador] = scanner.nextInt();
+            contador=contador+1;
+        }
+                
+        arreglo.mostrarElementosDelArreglo();
+        arreglo.mostrarMayorNum();
+        arreglo.mostrarMenorNum();
+        arreglo.calcularPromedio();
+    }
     
 }
