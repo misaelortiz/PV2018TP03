3890e0ccb7610621f9a1724e3f849b188dff06f1
E. Misael Ortiz <E. Misael Ortiz@VAIO>
2018-04-17 19:51:55 -0300

    Punto 8 Terminado.

--- src/tp3/punto9/TestBusqueda.java
+++ src/tp3/punto9/TestBusqueda.java
@@ -6,6 +6,7 @@
 package tp3.punto9;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 public class TestBusqueda {
     public static void main(String[] args) {
         ArrayList<Libro> libros  = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
        Libro libro1 = new Libro();
        Libro libro2 = new Libro();
        Libro libro3 = new Libro();
@@ -27,7 +29,7 @@ public class TestBusqueda {
        libros.add(libro1);
        
        libro2.setIsbn(1528452);
-       libro2.setTitulo("Entre dos Universos");
+       libro2.setTitulo("Entredosuniversos");
        libro2.setAutor("Andrea Tome");
        libro2.setPrecio(470);
        libro2.setEditorial("Plataforma NEO");
@@ -41,14 +43,34 @@ public class TestBusqueda {
        libros.add(libro3);
        
        libro4.setIsbn(999998);
-       libro4.setTitulo("El Juego Infinito");
+       libro4.setTitulo("Eljuegoinfinito");
        libro4.setAutor("John Dashner");
        libro4.setPrecio(175.50);
        libro4.setEditorial("Montena");
        libros.add(libro4);
+       /*Metodo para mostrar el ArrayList */
+        //libro1.visualizarLibros(libros);
        
-       libro1.visualizarLibros(libros);
-       
-       
+        System.out.println("Que libro desea buscar? Ingrese un titulo");
+        String buscado;
+        buscado=sc.next();
+        
+        /* Busqueda de un libro en el ArrayList 
+           En caso de que exista mostrara lo correspondiente.
+           De otro modo mostrara un mensaje de que el libro no existe.
+        */
+        
+        
+        for(int i = 0 ; i < libros.size();i++) {
+            if(libros.get(i).getTitulo().equals(buscado)==true) {
+                //System.out.println("Si existe");
+                System.out.println("ISBN: " + libros.get(i).getIsbn());
+                System.out.println("AUTOR: " +libros.get(i).getAutor());
+                System.out.println("PRECIO: " +libros.get(i).getPrecio());
+                System.out.println("EDITORIAL: " +libros.get(i).getEditorial());
+                } 
+                  
+            }   
+        System.out.println("Este libro no existe");
     }
 }
