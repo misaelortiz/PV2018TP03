f0a3bbdd91f18af624d5ca94fcd2d9e806422e71
E. Misael Ortiz <E. Misael Ortiz@VAIO>
2018-04-18 00:11:56 -0300

    Punto 8. Solucionado el problema que teniamos para mostrar bien los datos cargados en el Array

--- src/tp3/punto8/test/TestLibro.java
+++ src/tp3/punto8/test/TestLibro.java
@@ -20,48 +20,49 @@ public class TestLibro {
         Libro autor = new Libro();
         Libro titulo = new Libro();
         Libro precio = new Libro();
-        Libro unLibro = new Libro();
+        
         Scanner sc = new Scanner(System.in);
-       
+        int resp;
+        do{
+            int op=0;
+             Libro unLibro = new Libro();
+             resp=gl.MenuRegistro(op);
+            switch(resp){
+                case 1:{         
+                    long codigo;
+                    System.out.println("Ingrese ISBN del libro ");
+                    codigo=sc.nextLong();
+                    unLibro.setIsbn(codigo);
+                    String libro;
+                    System.out.println("Ingrese titulo del libro ");
+                    libro=sc.next();
+                    unLibro.setTitulo(libro);    
+                    String at;
+                    System.out.println("Ingrese autor del libro ");
+                    at=sc.next();
+                    unLibro.setAutor(at);
+                    double pc;
+                    System.out.println("Ingrese precio del libro ");
+                    pc=sc.nextDouble();
+                    unLibro.setPrecio(pc);
+                    
         
-        long codigo;
-        System.out.println("Ingrese ISBN del libro ");
-        codigo=sc.nextLong();
-        unLibro.setIsbn(codigo);
-       
-               
-        String libro;
-        System.out.println("Ingrese titulo del libro ");
-        libro=sc.next();
-        unLibro.setTitulo(libro);    
+                    gl.agregarLibro(unLibro);
+                    for(Libro l: gl.getLibros()){
+                    System.out.println(" " + l.getIsbn());
+                    System.out.println(" " + l.getTitulo());
+                    System.out.println(" " + l.getAutor());
+                    System.out.println(" " + l.getPrecio());
         
-        
-        String at;
-        System.out.println("Ingrese autor del libro ");
-        at=sc.next();
-        unLibro.setAutor(at);
-    
+                     }
+                }break;
+                default: System.out.println("fin del regristo");
                 
-        double pc;
-        System.out.println("Ingrese precio del libro ");
-        pc=sc.nextDouble();
-        unLibro.setPrecio(pc);
+            }
         
-        gl.agregarLibro(unLibro);
-       
-        
-         for(Libro l: gl.getLibros()){
-              System.out.println(" " + l.getIsbn());
-             System.out.println(" " + l.getTitulo());
-            System.out.println(" " + l.getAutor());
-            System.out.println(" " + l.getPrecio());
-        
-         }
-  
-        
-    }
+    }while(resp!=2);
          
-           
+    }         
         
     
 
