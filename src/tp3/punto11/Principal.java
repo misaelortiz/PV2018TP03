00c746dbbc88226d270cba4de40ad4d6612a8e22
E. Misael Ortiz <E. Misael Ortiz@VAIO>
2018-04-17 23:57:02 -0300

    Punto 11. Terminado

--- src/tp3/punto11/Principal.java
+++ src/tp3/punto11/Principal.java
@@ -15,31 +15,31 @@ public class Principal {
     public static void main(String[] args) {
         ArrayList<Zapatilla> zapatilla = new ArrayList<Zapatilla>();
         Scanner sc = new Scanner(System.in);
-        Zapatilla zapa=new Zapatilla();
+        //Zapatilla zapa=new Zapatilla();
         //Zapatilla marca=new Zapatilla();
         //Zapatilla ori = new Zapatilla();
         int resp;
         do{ 
             int op=0;
+            Zapatilla zapa=new Zapatilla();
             resp=zapa.MenuRegistro(op);
             
             switch(resp){
+                
                 case 1: {System.out.println("Ingrese marca de zapatilla:");                
                 zapa.setMarca(sc.next());
-                zapatilla.add(zapa);         
                 System.out.println("¿Es original?");
                 zapa.setOriginal(sc.next());
-                zapatilla.add(zapa); 
+                
+                    zapatilla.add(zapa); 
+                
                 }break;
                 case 2:  {
-                       // for(int i = 0 ; i < zapatilla.size(); i++) {           
-                       //System.out.println("Marca: " + zapatilla.get(i).getMarca());
-                       //System.out.println("Original: " +zapatilla.get(i).getOriginal());
-              
-                        
-                //} 
-                  
-                zapa.visualizarZapatillas(zapatilla);
+                       for(int i = 0 ; i < zapatilla.size(); i++) {           
+                       System.out.println("Marca: " + zapatilla.get(i).getMarca());
+                       System.out.println("Original: " + zapatilla.get(i).getOriginal());
+                        } 
+                        //zapa.visualizarZapatillas(zapatilla);
                 
                 }break;
                 
